8d962cd5523abb432e51fcd92b5e2c0d81919cdd
Matthew Wilbern <39929480+fatboychummy@users.noreply.github.com>
2024-01-10 12:14:55 -0700

    Create basic-floor-builder.lua

---
+++ basic-floor-builder.lua
@@ -0,0 +1,29 @@
+local function get_item()
+  for i = 1, 16 do
+    if turtle.getItemCount(i) > 0 then
+      turtle.select(i)
+      return
+    end
+  end
+  error("Out of items", 0)
+end
+
+local n = 0
+while true do
+  while turtle.back() do
+    get_item()
+    turtle.place()
+    n = 0
+  end
+
+  turtle.turnRight()
+  n = n + 1
+  if n >= 4 then
+    if ... then
+      turtle.down()
+    else
+      turtle.up()
+    end
+    return
+  end
+end
