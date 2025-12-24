83fce7701e2ca07f0ea2bb0e17f53156b6fcf5c2
Matthew Wilbern <39929480+fatboychummy@users.noreply.github.com>
2025-12-23 20:17:36 -0700

    Refactor, as well as implement editing computer names.

--- launcher.lua
+++ launcher.lua
@@ -87,8 +87,9 @@ main_win.clear()
 local selector = '>'
 local selected = 1
 local scroll_index = 0
-local computer_data = require "computer_data"
+local computer_data = require "computer_data" --[[@as table<integer, string>]]
 
+---@type {id: number, name: string}[]
 local selections = {}
 for id, name in pairs(computer_data) do
   table.insert(selections, { id = id, name = name })
@@ -106,11 +107,50 @@ end
 
 
 
+local function rewrite_data_file()
+  -- Write the computer_data.lua file.
+  local file, err = fs.open("computer_data.lua", "w")
+  if not file then
+    error("Failed to open computer_data.lua for writing: " .. err, 0)
+  end
+  file.write(
+    "return " .. textutils.serialize(computer_data)
+  )
+  file.close()
+end
+
+
 local mww, mwh = main_win.getSize()
 
 
 
-local function new_computer()
+--- Display a message in the main window for a set duration.
+---@param color number The text color of the message.
+---@param text string The message to display.
+---@param duration number? The duration to display the message for. Defaults to 2 seconds.
+local function message(color, text, duration)
+  main_win.setBackgroundColor(pal.crust)
+  main_win.setTextColor(color)
+
+  main_win.setCursorPos(3, mwh - 2)
+  main_win.write(text)
+
+  sleep(duration or 2)
+end
+
+
+
+--- Display an input page in the main window, prompting the user for information.
+---@param title string The title of the input page.
+---@param right_title string Displayed right-aligned in yellow on the same line as the title.
+---@param texts string[] The messages to display to the user.
+---@param response_is_number false Whether or not the response must be a number.
+---@param can_be_nil false Whether or not the response can be nil/empty. Only checked if `response_is_number` is true.
+---@return string response The user's response.
+---@overload fun(title: string, right_title: string, texts: string[], response_is_number: true, can_be_nil: true): number?
+---@overload fun(title: string, right_title: string, texts: string[], response_is_number: false, can_be_nil: true): string?
+---@overload fun(title: string, right_title: string, texts: string[], response_is_number: true, can_be_nil: false): number
+local function input_page(title, right_title, texts, response_is_number, can_be_nil)
   sleep() -- clear the char event from the event queue.
 
   main_win.setBackgroundColor(pal.surface_1)
@@ -119,26 +159,27 @@ local function new_computer()
   main_win.setCursorPos(1, 1)
   main_win.setBackgroundColor(pal.overlay_1)
   main_win.clearLine()
-  main_win.write(" New Computer Wizard")
+  main_win.write(" " .. title)
 
-  main_win.setCursorPos(mww - 3, 1)
+  main_win.setCursorPos(mww - #right_title - 1, 1)
   main_win.setTextColor(pal.yellow)
-  main_win.write('1/2')
-  main_win.setTextColor(pal.text)
+  main_win.write(right_title)
 
+  main_win.setTextColor(pal.text)
   main_win.setBackgroundColor(pal.surface_1)
-  main_win.setCursorPos(2, 3)
-  main_win.write("Enter desired computer ID.")
-  main_win.setCursorPos(2, 4)
-  main_win.write("Leave empty for a random ID.")
+  for i, text in ipairs(texts) do
+    main_win.setCursorPos(2, 2 + i)
+    main_win.write(text)
+  end
+
   local ypos = mwh - 2
   main_win.setBackgroundColor(pal.crust)
   draw_box(2, ypos - 1, mww - 2, 3, main_win)
-
   local write_win = window.create(main_win, 3, ypos, mww - 4, 1)
   write_win.setBackgroundColor(pal.crust)
   write_win.setTextColor(pal.blue)
   write_win.clear()
+
   local old = term.redirect(write_win)
 
   local function write_message(color, message)
@@ -150,90 +191,79 @@ local function new_computer()
     write_win.setTextColor(pal.blue)
   end
 
-  local id
+  local response
   while true do
     write_win.clear()
     write_win.setCursorPos(1, 1)
     local input = read()
 
-    if input == "" then
-      repeat
-        id = math.random(1, 65535)
-      until not computer_data[id]
-
-      write_message(pal.green, "Assigned random ID: " .. id)
-    elseif tonumber(input) then
-      id = tonumber(input)
-    else
-      write_message(pal.red, "Invalid ID. Please enter a number.")
-    end
-
-    if id then
-      if computer_data[id] then
-        write_message(pal.red, "ID already in use. Please choose another.")
-      else
+    if response_is_number then
+      if tonumber(input) then
+        response = tonumber(input)
         break
+      elseif input == "" and can_be_nil then
+        response = nil
+        break
+      else
+        message(pal.red, "Invalid input. Please enter a number.")
       end
-    end
-  end
-  ---@cast id -nil
-
-  -- what even is code reuse anyways
-  main_win.setBackgroundColor(pal.surface_1)
-  main_win.setTextColor(pal.text)
-  main_win.clear()
-  main_win.setCursorPos(1, 1)
-  main_win.setBackgroundColor(pal.overlay_1)
-  main_win.clearLine()
-  main_win.write(" New Computer Wizard")
-
-  main_win.setCursorPos(mww - 3, 1)
-  main_win.setTextColor(pal.yellow)
-  main_win.write('2/2')
-  main_win.setTextColor(pal.text)
-
-  main_win.setBackgroundColor(pal.surface_1)
-  main_win.setCursorPos(2, 3)
-  main_win.write("Enter desired computer name.")
-  main_win.setCursorPos(2, 4)
-  main_win.write("Leave empty for a random name.")
-  local ypos = mwh - 2
-  main_win.setBackgroundColor(pal.crust)
-  draw_box(2, ypos - 1, mww - 2, 3, main_win)
-
-  local name
-  while true do
-    write_win.clear()
-    write_win.setCursorPos(1, 1)
-    local input = read()
-
-    if input == "" then
-      name = random_names[math.random(1, #random_names)]
-      write_message(pal.green, "Assigned random name: " .. name)
     else
-      name = input
-    end
-
-    if name then
+      response = input
       break
     end
   end
 
   term.redirect(old)
 
+  return response
+end
+
+
+
+local function new_computer()
+  local id
+  repeat
+    id = input_page(
+      "New Computer Wizard",
+      "1/2",
+      {
+        "Enter desired computer ID.",
+        "Leave empty for a random ID."
+      },
+      true,
+      true
+    )
+
+    if not id then
+      repeat
+        id = math.random(1, 65535)
+      until not computer_data[id]
+      message(pal.green, "Assigned random ID: " .. id)
+    elseif computer_data[id] then
+      message(pal.red, "ID already in use. Please choose another.")
+    end
+  until id and not computer_data[id]
+
+  local name = input_page(
+    "New Computer Wizard",
+    "2/2",
+    {
+      "Enter desired computer name.",
+      "Leave empty for a random name."
+    },
+    false,
+    true
+  )
+  if name == "" then
+    name = random_names[math.random(1, #random_names)]
+    message(pal.green, "Assigned random name: " .. name)
+  end
+
   computer_data[id] = name
   table.insert(selections, { id = id, name = name })
   table.sort(selections, function(a, b) return a.id < b.id end)
 
-  -- Write the computer_data.lua file.
-  local file, err = fs.open("computer_data.lua", "w")
-  if not file then
-    error("Failed to open computer_data.lua for writing: " .. err, 0)
-  end
-  file.write(
-    "return " .. textutils.serialize(computer_data)
-  )
-  file.close()
+  rewrite_data_file()
 end
 
 
@@ -257,6 +287,38 @@ local function select_computer()
     end
 
     sleep(3)
+  end
+end
+
+local function edit_computer()
+  local selection = scroll_index + selected
+  if selections[selection] then
+    local id = selections[selection].id
+
+    local name = input_page(
+      "Edit Computer",
+      "",
+      {
+        ("Current name: %s"):format(computer_data[id]),
+        "Enter new name for computer ID " .. id .. ".",
+        "Leave empty to keep current name."
+      },
+      false,
+      true
+    )
+
+    if name ~= "" then
+      computer_data[id] = name
+      for _, entry in ipairs(selections) do
+        if entry.id == id then
+          entry.name = name
+          break
+        end
+      end
+      message(pal.green, "Updated computer name.")
+    end
+
+    rewrite_data_file()
   end
 end
 
@@ -301,39 +363,59 @@ local function refresh()
   term.write(("Sel: %d | Scroll: %d | Total: %d"):format(selected, scroll_index, #selections))--]]
 end
 
-while true do
-  refresh()
-  local _, key = os.pullEvent("key")
+local keys_held = {}
 
-  if key == keys.up or key == keys.w then
-    if selected > 1 then
-      selected = selected - 1
-    elseif scroll_index > 0 then
-      scroll_index = scroll_index - 1
-    else
-      -- At the top, rotate around.
-      scroll_index = math.max(0, #selections - mwh)
-      selected = math.min(mwh, #selections)
+local function modifiers()
+  while true do
+    local event, key = os.pullEvent()
+
+    if event == "key_up" then
+      keys_held[key] = nil
+    elseif event == "key" then
+      keys_held[key] = true
     end
-  elseif key == keys.down or key == keys.s then
-    if scroll_index + selected < #selections then
-      if selected < mwh then
-        selected = selected + 1
-      else
-        scroll_index = scroll_index + 1
-      end
-    else
-      -- At the bottom, rotate around.
-      scroll_index = 0
-      selected = 1
-    end
-  elseif key == keys.enter or key == keys.space then
-    select_computer()
-  elseif key == keys.n then
-    new_computer()
-  elseif key == keys.q then
-    break
   end
 end
+
+local function main()
+  while true do
+    refresh()
+    local _, key = os.pullEvent("key")
+
+    if key == keys.up or key == keys.w then
+      if selected > 1 then
+        selected = selected - 1
+      elseif scroll_index > 0 then
+        scroll_index = scroll_index - 1
+      else
+        -- At the top, rotate around.
+        scroll_index = math.max(0, #selections - mwh)
+        selected = math.min(mwh, #selections)
+      end
+    elseif key == keys.down or key == keys.s then
+      if scroll_index + selected < #selections then
+        if selected < mwh then
+          selected = selected + 1
+        else
+          scroll_index = scroll_index + 1
+        end
+      else
+        -- At the bottom, rotate around.
+        scroll_index = 0
+        selected = 1
+      end
+    elseif key == keys.enter or key == keys.space then
+      select_computer()
+    elseif key == keys.n and keys_held[keys.leftShift] then
+      new_computer()
+    elseif key == keys.e and keys_held[keys.leftShift] then
+      edit_computer()
+    elseif key == keys.q and keys_held[keys.leftShift] then
+      break
+    end
+  end
+end
+
+parallel.waitForAny(main, modifiers)
 
 catppuccin.reset_palette()
